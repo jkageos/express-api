2cc424291562c4487068217c23d7521639398476
Jonas Kong <jonas_kong@outlook.com>
2024-06-27 12:14:14 +0200

    format

--- src/mongoose/schemas/user.mjs
+++ src/mongoose/schemas/user.mjs
@@ -1,36 +1,14 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
-  bookmarks: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Blog",
-    },
-  ],
-  upvotedBlogs: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Blog",
-    },
-  ],
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
+  bookmarks: [{ type: Schema.Types.ObjectId, ref: "Blog" }],
+  upvotedBlogs: [{ type: Schema.Types.ObjectId, ref: "Blog" }],
 });
 
 export const User = mongoose.model("User", UserSchema);
